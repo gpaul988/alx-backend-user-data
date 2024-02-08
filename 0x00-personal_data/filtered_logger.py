8b6ab935a15da79fb8f0fa1dc68a033c6db34814
gpaul988 <gpaul988gmail.com>
2024-02-09 00:32:24 +0100

    fixed

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -44,11 +44,9 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
     host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user,
-                                   password=passwd,
-                                   host=host,
-                                   database=db_name)
-    return conn
+    return mysql.connector.connect(
+        user=user, password=passwd, host=host, database=db_name
+    )
 
 
 def main():
